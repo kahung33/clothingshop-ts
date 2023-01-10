53b1d571624f8022741fd601bc075d7a964c8a9b
HenryAUYEUNG <kahung930809@gmail.com>
2023-01-09 22:47:35 -0700

    typed categories in store

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,21 +1,25 @@
 import { createSelector } from 'reselect';
 
-const selectCategoriesReducer = (state) => state.categories;
+import { CategoriesState } from './category.reducer';
+
+import { CategoryMap } from "./category.types";
+
+const selectCategoriesReducer = (state): CategoriesState => state.categories;
 
 export const selectCategories = createSelector(
-    [selectCategoriesReducer],
+    [selectCategoriesReducer], //selector
     (categoriesSlice) => categoriesSlice.categories
 );
 
 export const selectCategoriesMap = createSelector(
     [selectCategories],
-    (categories) => 
+    (categories) : CategoryMap => 
     categories.reduce((acc, category) => {
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         console.log(acc);
         return acc;
-    } , {})
+    } , {} as CategoryMap)
 );
 
 export const selectCategoriesIsLoading = createSelector(
